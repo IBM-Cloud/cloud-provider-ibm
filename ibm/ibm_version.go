23a41f397c634be42ed9c340226246fec5d31fa9
iksrochpub <iksrochpub@ibm.com>
2021-09-19 00:00:27 +0000

    Automated repo sync 20210919
    Update from v1.22.1 to v1.22.2 (#2023)
    Update community to v0.22.2 (#2043)

--- ibm/ibm_version.go
+++ ibm/ibm_version.go
@@ -31,7 +31,7 @@ var (
 )
 
 // Version is overwritten during builds.
-var Version = "v1.22.1"
+var Version = "v1.22.2"
 
 func PrintVersionAndExitIfRequested() {
 	if versionFlag {
--- main.go
+++ main.go
@@ -18,8 +18,8 @@
 *******************************************************************************/
 
 // References:
-// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.22.1/staging/src/k8s.io/cloud-provider/app/controllermanager.go
-// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.22.1/staging/src/k8s.io/cloud-provider/sample/basic_main.go
+// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.22.2/staging/src/k8s.io/cloud-provider/app/controllermanager.go
+// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.22.2/staging/src/k8s.io/cloud-provider/sample/basic_main.go
 
 package main
 
