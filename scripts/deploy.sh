56c1b060f7bdd7dd7f5aa5f2d9fb7892165a3ed8
iksrochpub <iksrochpub@ibm.com>
2022-03-26 02:00:25 +0000

    Automated repo sync 20220326
    Require BOM PRs for IBM CCM image updates (#2571)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ******************************************************************************
 # IBM Cloud Kubernetes Service, 5737-D43
-# (C) Copyright IBM Corp. 2019, 2021 All Rights Reserved.
+# (C) Copyright IBM Corp. 2019, 2022 All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache2.0
 #
@@ -21,16 +21,6 @@ set -xe
 
 DOCKER_IMAGE_NAME="${1}"
 DOCKER_IMAGE_TAG="${2}"
-BUILD_PIPELINE_TAG="${3}"
 
 # Push the docker image.
 ./build-tools/docker/pushDockerImage.sh "${DOCKER_IMAGE_NAME}" "${DOCKER_IMAGE_TAG}"
-
-# Update the pipeline to use the docker image.
-git clone --depth=1 --no-single-branch "https://${GHE_USER}:${GHE_TOKEN}@github.ibm.com/alchemy-containers/armada-ansible.git"
-BOM_IMAGE="k8s_cloud_controller_manager_image"
-BOM_IMAGE_TAG="${DOCKER_IMAGE_TAG}"
-BOM_FILE_NAME=$(echo "${BUILD_PIPELINE_TAG}" | awk -F'[v.]' '{ print "armada-ansible-bom-"$2"."$3".yml" }')
-echo "Updating BOM ${BOM_FILE_NAME} image ${BOM_IMAGE} with new tag ${BOM_IMAGE_TAG} ..."
-export BOM_FILE_NAME
-armada-ansible/common/bom/tools/update-bom-image-tags.sh "${BOM_IMAGE}" "${BOM_IMAGE_TAG}"
