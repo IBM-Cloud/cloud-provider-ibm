36bfb24c7289f1ea0be98f5a5ba036d640041cb3
iksrochpub <iksrochpub@ibm.com>
2022-04-01 02:02:11 +0000

    Automated repo sync 20220401
    Update community to v0.24.0-beta.0 (#2616)
    Add networking squad as code owners (release 1.24) (#2519)
    Auto-create armada-ansible BOM PR (release 1.24) (#2582)

--- go.mod
+++ go.mod
@@ -9,11 +9,11 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.7.1
 	gopkg.in/gcfg.v1 v1.2.3
-	k8s.io/api v0.24.0-alpha.4
-	k8s.io/apimachinery v0.24.0-alpha.4
-	k8s.io/client-go v0.24.0-alpha.4
-	k8s.io/cloud-provider v0.24.0-alpha.4
-	k8s.io/component-base v0.24.0-alpha.4
+	k8s.io/api v0.24.0-beta.0
+	k8s.io/apimachinery v0.24.0-beta.0
+	k8s.io/client-go v0.24.0-beta.0
+	k8s.io/cloud-provider v0.24.0-beta.0
+	k8s.io/component-base v0.24.0-beta.0
 	k8s.io/klog/v2 v2.60.1
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9
 )
@@ -25,7 +25,7 @@ require (
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
@@ -73,9 +73,9 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
-	go.etcd.io/etcd/api/v3 v3.5.0 // indirect
-	go.etcd.io/etcd/client/pkg/v3 v3.5.0 // indirect
-	go.etcd.io/etcd/client/v3 v3.5.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.1 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.1 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.1 // indirect
 	go.mongodb.org/mongo-driver v1.7.5 // indirect
 	go.opentelemetry.io/contrib v0.20.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.20.0 // indirect
@@ -109,10 +109,10 @@ require (
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
-	k8s.io/apiserver v0.24.0-alpha.4 // indirect
-	k8s.io/component-helpers v0.24.0-alpha.4 // indirect
-	k8s.io/controller-manager v0.24.0-alpha.4 // indirect
-	k8s.io/kube-openapi v0.0.0-20220316025549-ddc66922ab18 // indirect
+	k8s.io/apiserver v0.24.0-beta.0 // indirect
+	k8s.io/component-helpers v0.24.0-beta.0 // indirect
+	k8s.io/controller-manager v0.24.0-beta.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.0.30 // indirect
 	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,3 +24,89 @@ DOCKER_IMAGE_TAG="${2}"
 
 # Push the docker image.
 ./build-tools/docker/pushDockerImage.sh "${DOCKER_IMAGE_NAME}" "${DOCKER_IMAGE_TAG}"
+
+# Initialize image, kube release, and tag information
+bom_image="k8s_cloud_controller_manager_image"
+current_release=$(grep "^TAG ?=" Makefile | awk '{ print $3 }')
+kube_major=$(echo "${current_release}" | cut -d'.' -f1 | tr -d 'v')
+kube_minor=$(echo "${current_release}" | cut -d'.' -f2)
+image_name=$(echo "${DOCKER_IMAGE_NAME}" | cut -d'/' -f2)
+new_image_tag=${DOCKER_IMAGE_TAG}
+
+# Clone the armada-ansible repo
+git clone --depth=1 --no-single-branch "https://${GHE_USER}:${GHE_TOKEN}@github.ibm.com/alchemy-containers/armada-ansible.git"
+
+cd armada-ansible/common/bom/next
+bom_file_list=$(grep "^${bom_image}:" ./* | grep ":v${kube_major}.${kube_minor}." | cut -d':' -f1)
+
+for file in $bom_file_list; do
+    echo "Updating BOM file ${file} image ${bom_image} with new tag ${new_image_tag} ..."
+
+    # Pull out the line that starts with the bom_image variable.
+    line=$(grep "^${bom_image}:" "${file}")
+
+    # Find the current image tag
+    prev_image_tag=$(echo "${line##*:}" | tr -d "'")
+
+    # Update the file
+    sed -i "s,${image_name}:${prev_image_tag},${image_name}:${new_image_tag}," "${file}"
+
+    # Display updated file
+    grep "^${bom_image}:" "${file}"
+
+    # Add file to the PR
+    git add "$file"
+done
+
+echo "Create new branch ..."
+git checkout -b "armdada-lb-${new_image_tag}"
+
+echo "Disaply changes for this PR..."
+git status
+
+# Determine the contents of the pull request message
+cd "${TRAVIS_BUILD_DIR}"
+if [[ "${DOCKER_IMAGE_TAG}" = dev-* ]]; then
+    dev_branch=${new_image_tag#"dev-"}
+    dev_branch=${dev_branch%-*}
+    pr_option="--draft"
+    echo "${DOCKER_IMAGE_TAG} is a dev image"
+    {
+        echo "DNM: Test BOM for ${image_name} - ${new_image_tag}"
+        echo
+        echo "### Do not merge. Test only."
+        echo
+        echo "To create TEST BOM, use Jenkins job: [armada-test-bom-publish](https://alchemy-containers-jenkins.swg-devops.com/job/Containers-Runtime/view/Armada-BOM/job/armada-test-bom-publish/build?delay=0sec)"
+        echo "with the following settings:"
+        echo
+        echo "- ARMADA_ANSIBLE_BRANCH: *armada-lb-${new_image_tag}*"
+        echo "- BOM_FILES: *select the BOM files changed in this PR*"
+        echo "- BOM_TYPE: *next*"
+        echo "- CARRIERS: *prestage-mon01-carrier1*"
+        echo
+        echo "### Commits to [${dev_branch}](https://github.ibm.com/alchemy-containers/armada-lb/commits/${dev_branch}) since ${prev_image_tag}"
+        echo
+        git log --no-patch --abbrev-commit --no-color --oneline "${prev_image_tag}"..."${new_image_tag}"
+        echo
+    } >"${TRAVIS_BUILD_DIR}"/message.txt
+else
+    kube_branch="release-${kube_major}.${kube_minor}"
+    pr_option=""
+    {
+        echo "Update ${image_name} to ${new_image_tag}"
+        echo
+        echo "### Commits to [armada-lb](https://github.ibm.com/alchemy-containers/armada-lb/commits/${kube_branch}) since ${prev_image_tag}"
+        echo
+        git log --no-patch --abbrev-commit --no-color "${prev_image_tag}"..."${new_image_tag}"
+        echo
+    } >"${TRAVIS_BUILD_DIR}"/message.txt
+fi
+cat "${TRAVIS_BUILD_DIR}"/armada-ansible/.github/pull_request_template.md >>"${TRAVIS_BUILD_DIR}"/message.txt
+cd ./armada-ansible
+
+echo "Comitting changes..."
+git commit --file "${TRAVIS_BUILD_DIR}"/message.txt
+
+echo "Creating pull request..."
+export GITHUB_TOKEN=${GHE_TOKEN}
+hub pull-request --file "${TRAVIS_BUILD_DIR}"/message.txt --push "${pr_option}"
