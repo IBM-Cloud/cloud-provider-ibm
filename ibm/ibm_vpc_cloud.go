3868205230ca90c5fab4cd39970e93083d02184b
iksrochpub <iksrochpub@ibm.com>
2024-03-11 07:00:37 +0000

    Automated repo sync 20240311
    Update module github.com/IBM/platform-services-go-sdk to v0.60.0 (#4159)
    Update module github.com/go-openapi/strfmt to v0.23.0 (#4160)
    Improve IKS/ROKS cluster create time (release 1.29) (#4139)
    CVE-2023-24786: google.golang.org/protobuf to v1.33.0 (release-1.29) (#4141)

--- ibm/ibm_vpc_cloud.go
+++ ibm/ibm_vpc_cloud.go
@@ -1,6 +1,6 @@
 /*******************************************************************************
 * IBM Cloud Kubernetes Service, 5737-D43
-* (C) Copyright IBM Corp. 2021, 2023 All Rights Reserved.
+* (C) Copyright IBM Corp. 2021, 2024 All Rights Reserved.
 *
 * SPDX-License-Identifier: Apache2.0
 *
@@ -24,6 +24,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 
@@ -36,6 +38,9 @@ import (
 // envVarPublicEndPoint is an environmental variable used to select public service endpoint
 // Accepted value is "true", if any other value is set, it will be ignored.
 const envVarPublicEndPoint = "ENABLE_VPC_PUBLIC_ENDPOINT"
+
+var CreateUpdateMutex = sync.Mutex{}     // Serialize access to sync channel. Only allow 1 thread to do close()
+var CreateUpdateChan = make(chan string) // Channel used to wake up sleeping CreateLB thread(s) when nodes are available
 
 // shouldPrivateEndpointBeEnabled - Determine if private service endpoint should be enabled
 func shouldPrivateEndpointBeEnabled() bool {
@@ -118,7 +123,27 @@ func (c *Cloud) VpcEnsureLoadBalancer(ctx context.Context, clusterName string, s
 	klog.Infof("EnsureLoadBalancer(lbName:%v, Service:{%v}, NodeCount:%v)", lbName, c.vpcGetServiceDetails(service), len(nodes))
 	if len(nodes) == 0 {
 		errString := "There are no available nodes for LoadBalancer"
-		klog.Errorf(errString)
+		// This routine was called when there are no nodes available.
+		// When we return an error, Kubernetes will go into an increasing exponential backoff retry logic.
+		// By the time that a cluster node finally appears, the wait time between attempts will likely be significant.
+		// To prevent this exponential delay from ramping up to the 5 min max, wait up to 10 min on each attempt.
+		syncChan := CreateUpdateChan
+		if syncChan == nil {
+			klog.Errorf("EnsureLoadBalancer: %s. Do NOT wait for nodes to become ready", errString)
+		} else {
+			klog.Warningf("EnsureLoadBalancer: %s. Wait for nodes to become ready", errString)
+			maxWait := 10
+			for i := 0; i < maxWait; i++ { // Wait for 10 min
+				select {
+				case <-syncChan:
+					klog.Infof("- nodes now available for EnsureLoadBalancer, return so that Kubernetes will retry")
+					return nil, fmt.Errorf(errString)
+				case <-time.After(time.Minute): // Display message every min
+					klog.Warningf("- no available nodes for load balancer (%d out %d)", i+1, maxWait)
+					continue
+				}
+			}
+		}
 		return nil, fmt.Errorf(errString)
 	}
 	vpc, err := c.InitCloudVpc(shouldPrivateEndpointBeEnabled())
@@ -202,7 +227,7 @@ func (c *Cloud) vpcGetServiceDetails(service *v1.Service) string {
 func (c *Cloud) VpcMonitorLoadBalancers(services *v1.ServiceList, status map[string]string) {
 	// If there are no load balancer services to monitor, don't even initCloudVpc, just return.
 	if services == nil || len(services.Items) == 0 {
-		klog.Infof("No Load Balancers to monitor, returning")
+		klog.Infof("MonitorLB: No Load Balancers to monitor, returning")
 		return
 	}
 	vpc, err := c.InitCloudVpc(shouldPrivateEndpointBeEnabled())
@@ -221,6 +246,21 @@ func (c *Cloud) VpcUpdateLoadBalancer(ctx context.Context, clusterName string, s
 		errString := "There are no available nodes for LoadBalancer"
 		klog.Errorf(errString)
 		return fmt.Errorf(errString)
+	}
+	// There could be one or more EnsureLoadBalancer threads blocked waiting for the cluster nodes to become available.
+	// Closing the sync channel will release all of the blocked EnsureLoadBalancer threads.
+	// There could be multiple threads running though this logic for different services.
+	// Ensure the the sync channel is only closed once by protecting this operation with a mutex.
+	// Finally, we don't want serialize on the mutex in the future, so set sync channel to nil after it has been closed
+	if CreateUpdateChan != nil {
+		CreateUpdateMutex.Lock()
+		syncChan := CreateUpdateChan
+		if syncChan != nil { // The chan may have been closed while we were waiting for the mutex.Lock()
+			klog.Infof("- UpdateLoadBalancer called with %d node(s), wake up any blocked EnsureLoadBalancer", len(nodes))
+			close(syncChan)
+			CreateUpdateChan = nil
+		}
+		CreateUpdateMutex.Unlock()
 	}
 	vpc, err := c.InitCloudVpc(shouldPrivateEndpointBeEnabled())
 	if err != nil {
