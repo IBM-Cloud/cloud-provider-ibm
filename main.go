bee3057f70610f841e06da3f453ddc6c7e46b7fb
iksrochpub <iksrochpub@ibm.com>
2024-06-15 06:00:36 +0000

    Automated repo sync 20240615
    Update community (#4382)
    Update from v1.28.10 to v1.28.11 (#4402)

--- main.go
+++ main.go
@@ -18,8 +18,8 @@
 *******************************************************************************/
 
 // References:
-// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.28.10/staging/src/k8s.io/cloud-provider/app/controllermanager.go
-// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.28.10/staging/src/k8s.io/cloud-provider/sample/basic_main.go
+// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.28.11/staging/src/k8s.io/cloud-provider/app/controllermanager.go
+// - https://raw.githubusercontent.com/kubernetes/kubernetes/v1.28.11/staging/src/k8s.io/cloud-provider/sample/basic_main.go
 
 package main
 
